76a9bf5c753a35a31bfb36bbedcccf9669a2d80d
evgep <evgepishcin@gmail.com>
2019-05-10 23:24:49 +0300

    Добавлено поле interlocutorSex

--- src/com/tagchat/Client.java
+++ src/com/tagchat/Client.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 public class Client {
     private Socket socket;
     private byte sex; // 0 - not set, 1 - male, 2 - female
+    private byte interlocutorSex; // 0 - not set, 1 - male, 2 - female
     private ArrayList<String> tagList;
     private boolean findAllTag; //search for all tag matches or search for one of the tags
 
-    public Client(Socket socket, byte sex, ArrayList<String> tagList, boolean findAllTag) {
+    public Client(Socket socket, byte sex, byte interlocutorSex, ArrayList<String> tagList, boolean findAllTag) {
         this.socket = socket;
         this.sex = sex;
+        this.interlocutorSex = interlocutorSex;
         this.tagList = tagList;
         this.findAllTag = findAllTag;
     }
