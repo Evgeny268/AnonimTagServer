fae6916511478612e4cb56bf82575d23ec4f72e1
evgep <evgepishcin@gmail.com>
2019-05-10 23:20:11 +0300

    в класс Transfer добавлены поля interlocutorSex и topTags

--- src/com/tagchat/Transfer.java
+++ src/com/tagchat/Transfer.java
@@ -1,22 +1,41 @@
 package com.tagchat;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Transfer {
     private String text = null;
     private byte sex = 0; // 0 - not set, 1 - male, 2 - female
+    private byte interlocutorSex = 0; // 0 - not set, 1 - male, 2 - female
     private ArrayList<String> tagList = null;
     private boolean findAllTag = false; //search for all tag matches or search for one of the tags
     private boolean adult = false;
+    private Map<String,Integer> topTags = null;
 
     public Transfer(String text) {
         this.text = text;
     }
 
-    public Transfer(byte sex, ArrayList<String> tagList, boolean findAllTag) {
+    public Transfer(byte sex, byte interlocutorSex, ArrayList<String> tagList, boolean findAllTag) {
+        if (sex<0 || sex>2){
+            sex = 0;
+            interlocutorSex = 0;
+        }
+        if (interlocutorSex<0 || interlocutorSex>2){
+            interlocutorSex = 0;
+        }
         this.sex = sex;
         this.tagList = tagList;
         this.findAllTag = findAllTag;
+        if (sex==0){
+            this.interlocutorSex = 0;
+        }else {
+            this.interlocutorSex = interlocutorSex;
+        }
+    }
+
+    public Transfer(Map<String, Integer> topTags) {
+        this.topTags = topTags;
     }
 
     public Transfer(boolean adult) {
@@ -31,6 +50,10 @@ public class Transfer {
         return sex;
     }
 
+    public byte getInterlocutorSex() {
+        return interlocutorSex;
+    }
+
     public ArrayList<String> getTagList() {
         return tagList;
     }
@@ -41,5 +64,9 @@ public class Transfer {
 
     public boolean isAdult() {
         return adult;
+    }
+
+    public Map<String, Integer> getTopTags() {
+        return topTags;
     }
 }
