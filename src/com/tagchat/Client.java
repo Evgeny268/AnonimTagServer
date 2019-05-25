57367eb53f76cd811090691a8ba80044b54d281a
evgep <evgepishcin@gmail.com>
2019-05-26 01:19:53 +0300

    +поля ObjectStream

--- src/com/tagchat/Client.java
+++ src/com/tagchat/Client.java
@@ -1,17 +1,23 @@
 package com.tagchat;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class Client {
     private Socket socket;
+    private ObjectInputStream ois = null;
+    private ObjectOutputStream oos = null;
     private byte sex; // 0 - not set, 1 - male, 2 - female
     private byte interlocutorSex; // 0 - not set, 1 - male, 2 - female
     private ArrayList<String> tagList;
     private boolean findAllTag; //search for all tag matches or search for one of the tags
 
-    public Client(Socket socket, byte sex, byte interlocutorSex, ArrayList<String> tagList, boolean findAllTag) {
+    public Client(Socket socket, ObjectOutputStream oos, ObjectInputStream ois,byte sex, byte interlocutorSex, ArrayList<String> tagList, boolean findAllTag) {
         this.socket = socket;
+        this.oos = oos;
+        this.ois = ois;
         this.sex = sex;
         this.interlocutorSex = interlocutorSex;
         this.tagList = tagList;
