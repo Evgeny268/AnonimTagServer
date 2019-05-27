bd2845377168e62c5cf3193616663ff6085e67a3
evgep <evgepishcin@gmail.com>
2019-05-27 23:02:59 +0300

    Создан класс Dialog принимающий двух клиентов

---
+++ src/com/tagchat/Dialog.java
@@ -0,0 +1,12 @@
+package com.tagchat;
+
+public class Dialog{
+    private Client client1;
+    private Client client2;
+
+    public Dialog(Client client1, Client client2) {
+        this.client1 = client1;
+        this.client2 = client2;
+    }
+
+}
