10224c2988d0ad953dc4d2cd4b7ae3091e4a9cf0
evgep <evgepishcin@gmail.com>
2019-05-30 15:22:35 +0300

    в коассе Dialog написан метод run

--- src/com/tagchat/Dialog.java
+++ src/com/tagchat/Dialog.java
@@ -1,6 +1,6 @@
 package com.tagchat;
 
-public class Dialog{
+public class Dialog extends Thread{
     private Client client1;
     private Client client2;
 
@@ -9,4 +9,24 @@ public class Dialog{
         this.client2 = client2;
     }
 
+    @Override
+    public void run() {
+        MessageListener m1 = new MessageListener(client1.getOis(),client2.getOos());
+        MessageListener m2 = new MessageListener(client2.getOis(),client1.getOos());
+        m1.start();
+        m2.start();
+        try {
+            m1.join();
+            m2.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            client1.close();
+            client2.close();
+            synchronized (ClientCreator.listLock){
+                ClientCreator.getClientList().remove(client1);
+                ClientCreator.getClientList().remove(client2);
+            }
+        }
+    }
 }
