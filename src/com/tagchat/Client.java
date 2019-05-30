21f9cde4b42f79c65405b68f3f701eb98745fd90
evgep <evgepishcin@gmail.com>
2019-05-30 15:21:46 +0300

    в классе Client дописаны геттары

--- src/com/tagchat/Client.java
+++ src/com/tagchat/Client.java
@@ -1,5 +1,6 @@
 package com.tagchat;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -22,5 +23,51 @@ public class Client {
         this.interlocutorSex = interlocutorSex;
         this.tagList = tagList;
         this.findAllTag = findAllTag;
+    }
+
+    public void close(){
+        try{
+            ois.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try{
+            oos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try{
+            socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public byte getSex() {
+        return sex;
+    }
+
+    public byte getInterlocutorSex() {
+        return interlocutorSex;
+    }
+
+    public ArrayList<String> getTagList() {
+        return tagList;
+    }
+
+    public boolean isFindAllTag() {
+        return findAllTag;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public ObjectInputStream getOis() {
+        return ois;
+    }
+
+    public ObjectOutputStream getOos() {
+        return oos;
     }
 }
