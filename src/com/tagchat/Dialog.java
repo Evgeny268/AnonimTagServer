05fed16a42c638dcda428edfb93643cf97942bfb
Evgeny268 <evgepishcin@gmail.com>
2019-07-01 01:11:52 +0300

    add log messages

--- src/com/tagchat/Dialog.java
+++ src/com/tagchat/Dialog.java
@@ -15,17 +15,20 @@ public class Dialog extends Thread{
         MessageListener m2 = new MessageListener(client2.getOis(),client1.getOos());
         m1.start();
         m2.start();
+        System.out.println("Создан новый диалог");
         try {
             m1.join();
             m2.join();
         }catch (InterruptedException e){
             e.printStackTrace();
+            System.err.println("Ошибка ожидания завершения диалога");
         }finally {
             client1.close();
             client2.close();
             synchronized (ClientCreator.listLock){
                 ClientCreator.getClientList().remove(client1);
                 ClientCreator.getClientList().remove(client2);
+                System.out.println("Удаляю клиентов из листа");
             }
         }
     }
